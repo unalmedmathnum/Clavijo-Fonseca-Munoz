cdaa53d051b26093487fe7e207f79bc1eaffb671
richardmozenao <rmunozh@unal.edu.co>
2024-12-06 18:07:35 -0500

    test desde directorio raiz para mas facilidad

---
+++ ejecutarTest.py
@@ -0,0 +1,12 @@
+#from Test.testPolynomialMethod import  testPolynomial
+from Test.testPowerMethod import testPowerMethod
+from Test.testQRMethod import  testQRMethod
+
+print("-----------------------------------------Método directo-----------------------------------------")
+#testPolynomial()
+
+print("-----------------------------------------Método de las potencias-----------------------------------------")
+testPowerMethod()
+
+print("-----------------------------------------Algoritmo QR-----------------------------------------")
+testQRMethod()
